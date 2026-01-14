1add3e56f101b7e64a8ecb467c60e6b6545b8c96
Kalpak <kalpakc404@gmail.com>
2026-01-15 01:07:57 +0530

    implemented fibonacci by watching tutorial

--- Oliver Booth/FibzBuzz.c
+++ Oliver Booth/FibzBuzz.c
@@ -1,3 +1,27 @@
 #include <stdio.h>
 
 
+
+void fib(int n)
+{
+	int result=0;
+	int a =0;
+	int b=1;
+	for(int i=0;i<n;i++)
+	{
+		printf(" %d ",a);
+		result=a+b;
+		b=a;
+		a=result;
+	}
+	
+}
+
+
+int main()
+{
+	int n = 10;
+	fib(n);
+	return 0;
+}
+
